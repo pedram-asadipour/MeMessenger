95d7b58efd3ce8060c971017ecc91b26c36aa0cb
pedram <pedram.asadipour@gmail.com>
2021-10-07 19:15:59 +0330

    Init Project

---
+++ ServerHost/Pages/Auth.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServerHost.Pages
+{
+    public class AuthModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ServerHost/wwwroot/js/auth.js
@@ -0,0 +1,34 @@
+const contactChats = $("#contact-chat-items");
+const Chats = $("#Chats");
+const currentUser = $("#user-detail .current-user");
+const userChat = $("#user-detail .user-chat");
+const chatsSection = document.querySelector("#Chats ul");
+
+
+$(function () {
+
+  //#region Go To End Scroll
+  chatsSection.scrollTop = chatsSection.scrollHeight;
+  //#endregion
+
+});
+
+function openChat(element) {
+  if (!contactChats.hasClass("disable-element")) {
+    contactChats.addClass("disable-element");
+    Chats.removeClass("disable-element");
+
+    currentUser.addClass("disable-element");
+    userChat.removeClass("disable-element");
+  }
+}
+
+function back() {
+  if (contactChats.hasClass("disable-element")) {
+    contactChats.removeClass("disable-element");
+    Chats.addClass("disable-element");
+
+    currentUser.removeClass("disable-element");
+    userChat.addClass("disable-element");
+  }
+}
